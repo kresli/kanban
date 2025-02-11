074843bd940d20ffdb5e0dd149a04b72f6ae004d
Eduard Jacko <jacko.eduard@gmail.com>
2025-02-11 13:26:19 +0000

    feat(CardDetailsModal): enhance ListSelector styling with icon and improved layout

--- src/components/CardDetailsModal/Sidebar.tsx
+++ src/components/CardDetailsModal/Sidebar.tsx
@@ -35,17 +35,22 @@ function ListSelector(props: { card: Card_Schema }) {
     api.updateCard({ ...props.card, listId: e.target.value });
   };
   return (
-    <select
-      value={props.card.listId}
-      onChange={onChange}
-      className="w-full appearance-none rounded border border-rim bg-primary-50 px-2 py-1 text-sm font-light text-primary-500 hover:bg-primary-100 focus:bg-primary-100 focus:outline-none active:bg-primary-100 active:outline-none"
-    >
-      {lists?.map((list) => (
-        <option key={list.id} value={list.id}>
-          {list.title}
-        </option>
-      ))}
-    </select>
+    <div className="relative flex w-full appearance-none flex-row items-center justify-center space-x-2 rounded border border-rim bg-primary-50 text-sm font-light text-primary-500 hover:bg-primary-100 focus:bg-primary-100 focus:outline-none active:bg-primary-100 active:outline-none">
+      <IconList size={14} className="absolute left-2" />
+      <div className="h-full w-full pl-5">
+        <select
+          value={props.card.listId}
+          onChange={onChange}
+          className="w-full cursor-pointer appearance-none rounded px-2 py-1 text-sm font-light text-primary-500 hover:bg-primary-100 focus:bg-primary-100 focus:outline-none active:bg-primary-100 active:outline-none"
+        >
+          {lists?.map((list) => (
+            <option key={list.id} value={list.id}>
+              {list.title}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
 
